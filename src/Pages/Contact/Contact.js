ecc88ea8210437ca701a32493c651e5455bfde02
LOGIC50 <ahmedjafrulislam@gmail.com>
2021-11-30 23:29:30 +0600

    update some page

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,11 +1,69 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from 'emailjs-com';
+import { Container, Grid } from '@mui/material';
+import { Box } from '@mui/material';
+import './Contact.css';
 
 const Contact = () => {
+    const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('gmail', 'template_tt4spyj', form.current, 'user_p0OAJuaEwDb1AvxTcaKEV')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset();
+  };
     return (
+        <>
         <div>
-            <h2>Contact Me</h2>
+            <h1 style={{'text-align': 'center', 'marginTop': '100px'}}>Contact With Me</h1>
         </div>
+        <div>
+        <Container className='home-portion'>
+            <Box sx={{ flexGrow: 1 }} className='contact-box'>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={6} className='contact-info'>
+          <div>
+          <img src="https://i.ibb.co/TLJbhCQ/1500-0.jpg" alt="" />
+          </div>
+          <div>
+            <h2>Ahmed Jafrul Islam</h2>
+            <h6>Front End Web Developer</h6>
+            <p>I am available for freelance work.</p>
+            <p>If needed, feel free to contact with me</p>
+            <p>Phone: <span>+8801406475446</span></p>
+            <p>Email: <span>ahmedjafrulislam@gmail.com</span></p>
+            <p>FIND WITH ME</p>
+          </div>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <form ref={form} onSubmit={sendEmail} className='contact-form'>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>PHONE NUMBER</label>
+      <input type="number" name="user_contact" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>SUBJECT</label>
+      <input type="text" name="subject" />
+      <label>MESSAGE</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+        </Grid>
+        
+      </Grid>
+    </Box>
+        </Container>
+            
+        </div>
+        </>
     );
 };
-
 export default Contact;
+
--- src/Pages/Skills/Skills.js
+++ src/Pages/Skills/Skills.js
@@ -1,11 +1,79 @@
-import React from 'react';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import { Container } from '@mui/material';
+
+const images = [
+    {
+        img: 'https://i.ibb.co/M53XdL3/1.png'
+    },
+    {
+        img: 'https://i.ibb.co/YLKQwNQ/2.png'
+    },
+    {
+        img: 'https://i.ibb.co/tbHRjz3/3.jpg'
+    },
+    {
+        img: 'https://i.ibb.co/cNnTFdj/4.png'
+    },
+    {
+        img: 'https://i.ibb.co/dLxjKyj/5.jpg'
+    },
+    {
+        img: 'https://i.ibb.co/6mFB9wt/6.png'
+    },
+    {
+        img: 'https://i.ibb.co/4K9XpTs/7.png'
+    },
+    {
+        img: 'https://i.ibb.co/CHtCVy5/8.png'
+    },
+    {
+        img: 'https://i.ibb.co/9q4Dqfk/9.jpg'
+    },
+    {
+        img: 'https://i.ibb.co/nwxVRsv/10.jpg'
+    },
+    {
+        img: 'https://i.ibb.co/7Xhhgp7/11.jpg'
+    },
+    {
+        img: 'https://i.ibb.co/7Xhhgp7/11.jpg'
+    }
+]
 
 const Skills = () => {
     return (
-        <div>
-            <h4>Skills Page</h4>
-        </div>
+        <Container>
+            <h1>Skills</h1>
+            <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+          {
+              images.map(image => <Grid style={{'border': '1px solid aquamarine', 'margin': '20px','alignItems': 'center'}} item xs={6} md={3}>
+                <img style={{'width': 'auto', 'margin': '20px'}} src={image.img} alt="" />
+              </Grid>)
+          }
+        
+        
+      </Grid>
+    </Box>
+        </Container>
     );
 };
 
 export default Skills;
+
+
+
+
+
+
+
+
+
+
+
+
+// https://i.ibb.co/VTPdnxH/12.jpg
+// https://i.ibb.co/WF4XjHB/13.png
+// https://i.ibb.co/X77Mwhj/14.jpg
